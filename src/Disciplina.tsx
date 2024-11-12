daa563195155f1e804ca07c087859c1b9b6d109a
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-11-11 22:32:34 -0300

    cria disciplina

---
+++ src/Disciplina.tsx
@@ -0,0 +1,13 @@
+function Disciplina(params:any) {
+    return (
+        <div>
+            <div>
+                DCC207
+            </div>
+            <div>
+                Desenvolvimento web
+            </div>
+        </div>
+    )
+}
+export default Disciplina;
