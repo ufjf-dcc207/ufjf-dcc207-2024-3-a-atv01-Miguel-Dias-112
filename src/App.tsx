5848ddf349c3f2d9ea6788bc6bec3670d77e3797
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-11-11 22:57:35 -0300

    cria implentação por lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
 import './App.css'
 import Disciplina from './Disciplina'
-
+type DisciplinaProps = {
+    codigo:string;
+    nome:string;
+}
 function App() {
   const [count, setCount] = useState(0)
-
+  
+  let disciplinas:DisciplinaProps[] = [
+    {codigo:'DCC206', nome:'Desenvolvimento Web 1'},
+    {codigo:'DCC206', nome:'Desenvolvimento Web 2'},
+    {codigo:'DCC206', nome:'Desenvolvimento Front'}
+  ]
   return (
     <>
      <h1>Hello World</h1>
-     <Disciplina codigo='NfaçoIdeia' nome='Algoritmos2'></Disciplina>
-     <Disciplina codigo='DCC206' nome='Desenvolvimento Web 1'></Disciplina>
-
-     <Disciplina codigo='DCC206' nome='Desenvolvimento Web 2'></Disciplina>
-     <Disciplina codigo='DCC206' nome='Desenvolvimento Front'></Disciplina>
+     
+    { 
+      disciplinas.map(
+        (disciplina)=>{
+          return <Disciplina codigo={disciplina.codigo} nome={disciplina.nome}/>
+        }
+      )
+    }
 
     </>
   )
