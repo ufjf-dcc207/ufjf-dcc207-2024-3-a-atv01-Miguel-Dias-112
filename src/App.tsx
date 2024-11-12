b68073d5d64c7c87673dd071072f4e3a544b9125
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-11-11 23:00:41 -0300

    adiciona estado

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,26 @@ type DisciplinaProps = {
     nome:string;
 }
 function App() {
-  const [count, setCount] = useState(0)
-  
-  let disciplinas:DisciplinaProps[] = [
+  let _disciplinas:DisciplinaProps[] = [
     {codigo:'DCC206', nome:'Desenvolvimento Web 1'},
     {codigo:'DCC206', nome:'Desenvolvimento Web 2'},
     {codigo:'DCC206', nome:'Desenvolvimento Front'}
   ]
+
+  const [disciplinas, setDisciplinas] = useState(_disciplinas)
+
+ 
+  
   return (
     <>
      <h1>Hello World</h1>
-     
+     <section>
+        <input type="text" />
+        <input type="text"/>
+        <input type="button" value="Increment"/>
+     </section>
+
+    
     { 
       disciplinas.map(
         (disciplina)=>{
